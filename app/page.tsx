f1b1a548d0a320c820d09b7c1240ccb0eb08f064
Pedro Hammes <predohammes@gmail.com>
2026-05-03 10:22:38 -0300

    feat: section 02 about

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Section01Banner from "@/components/sections/section01Banner";
+import Section02Banner from "@/components/sections/section02Abbout";
 
 export default function Home() {
+
   return (
     <main>
       <Section01Banner />
+      <Section02Banner />
     </main>
   );
 }
---
+++ components/sections/section02Abbout.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+import { Diamond } from "lucide-react"
+
+export default function Section02Banner() {
+    const items = [
+        { title: "GERENCIAMENTO COMPLETO DE REDES SOCIAIS", description: "Calendário de conteúdos; briefing de conteúdos, relatório de métricas mensais e trimestrais com cálculo automático de taxas de engajamento e de conversão; funil de vendas; pilares de conteúdos." },
+        { title: "UNIVERSO DA MARCA", description: "Identidade visual; análise de mercado; brand persona; público alvo; jornada do consumidor e funil de vendas da empresa; comunicação; estratégia digital." },
+        { title: "ESPAÇO CLIENTE", description: "O template ainda conta com um espaço do cliente, com comprovantes de pagamento, contrato de prestação de serviços e dados de logins de contas" },
+    ]
+    return (
+        <section className='relative flex flex-col gap-4 items-center justify-center px-8'>
+            <div className="w-90 h-125 relative rounded-[4rem] overflow-hidden">
+                <Image 
+                src={"/ux/section-02-about/section-02-about.webp"}
+                alt="Sobre a empresa"
+                fill
+                className="object-cover"
+                />
+            </div>
+            
+            <h2 className="font-heading text-5xl text-primary">O sistema mais completo do mercado</h2>
+            <div className="flex flex-col gap-8">
+                {items.map((item, index) => (
+                    <div key={index} className="
+                    flex flex-col py-4 gap-2 justify-center items-center
+                    border-b border-foreground">
+                        <Diamond color="#a15035"/>
+                        <h3 className="font-bold text-base mb-2">{item.title}</h3>
+                        <p className="text-foreground">{item.description}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
