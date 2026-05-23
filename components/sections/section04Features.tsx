0867c41b8be3c174a4d37ca4a91e8d52b6de1063
Pedro Hammes <predohammes@gmail.com>
2026-05-23 11:49:11 -0300

    style: layout responsivo

--- components/sections/section04Features.tsx
+++ components/sections/section04Features.tsx
@@ -2,22 +2,22 @@ import Image from "next/image"
 
 export default function Section04Features() {
     return(
-        <section className='relative flex flex-col gap-4 items-center justify-center px-8 '>
+        <section className='flex flex-col lg:flex-row gap-8 items-center justify-center px-8'>
             {/* Texto */}
-            <div className="flex flex-col">
-                <h2 className="font-heading text-5xl text-primary">Otimize tempo nos relatórios de redes sociais</h2>
-                <p className="font-sans text-foreground text-base mt-4">Um espaço dedicado a armazenar os relatórios das redes sociais.</p>
-                <p className="font-sans text-foreground text-base mt-4">_Relatórios mensais</p>
-                <p className="font-sans text-foreground text-base">_Relatórios trimestrais</p>
-                <p className="font-sans text-foreground text-base">_Relatórios anuais</p>
+            <div className="flex flex-col lg:w-1/2">
+                <h2 className="font-heading text-5xl lg:text-6xl text-primary">Otimize tempo nos relatórios de redes sociais</h2>
+                <p className="font-sans text-foreground text-base lg:text-2xl mt-4">Um espaço dedicado a armazenar os relatórios das redes sociais.</p>
+                <p className="font-sans text-foreground text-base lg:text-2xl mt-4">_Relatórios mensais</p>
+                <p className="font-sans text-foreground text-base lg:text-2xl">_Relatórios trimestrais</p>
+                <p className="font-sans text-foreground text-base lg:text-2xl">_Relatórios anuais</p>
             </div>
 
             {/* Imagem */}
-            <div>
+            <div className="lg:w-1/2 flex justify-center">
                 <div className="w-90 h-125 relative rounded-[4rem] overflow-hidden">
                     <Image 
                     src={"/ux/section-04-features/relatorios.webp"}
-                    alt="Sobre a empresa"
+                    alt="Relatórios de redes sociais"
                     fill
                     className="object-cover"
                     />
