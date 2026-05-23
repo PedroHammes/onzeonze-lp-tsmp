9d00c7b1face25aff2bd62a7e0272cf8d7ef7107
Pedro Hammes <predohammes@gmail.com>
2026-05-23 12:13:29 -0300

    style: layout responsivo desktop

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Section02Banner from "@/components/sections/section02Abbout";
 import Section03Differentiators from "@/components/sections/section03Differentiators";
 import Section04Features from "@/components/sections/section04Features";
 import Section05Details from "@/components/sections/section05Details";
-import Section06Audicence from "@/components/sections/section06Audience";
+import Section06Audience from "@/components/sections/section06Audience";
 import Section07Payment from "@/components/sections/section07Payment";
 import Section08Creator from "@/components/sections/section08Creator";
 import Section09Video from "@/components/sections/section09Video";
@@ -17,7 +17,7 @@ export default function Home() {
       <Section03Differentiators />
       <Section04Features />
       <Section05Details />
-      <Section06Audicence />
+      <Section06Audience />
       <Section07Payment />
       <Section08Creator />
       <Section09Video />
--- components/sections/section07Payment.tsx
+++ components/sections/section07Payment.tsx
@@ -3,28 +3,28 @@ import Image from "next/image";
 
 export default function Section07Payment() {
     return (
-        <section className='relative flex flex-col gap-4 items-center justify-center px-8 bg-secondary'>
+        <section className='flex flex-col lg:flex-row gap-8 items-center justify-center px-8 lg:px-16 bg-secondary'>
             {/* Texto */}
-            <div className="flex flex-col gap-4 items-start">
-                <p className="font-sans text-foreground text-base">
+            <div className="flex flex-col gap-4 lg:w-1/2">
+                <p className="font-sans text-foreground text-base lg:text-2xl">
                     Comece agora mesmo a gerenciar com mais eficiência e organização
                 </p>
-                <h2 className="font-heading text-primary text-5xl">
+                <h2 className="font-heading text-primary text-5xl lg:text-6xl">
                     Pagamento único, acesso imediato e vitalício
                 </h2>
                 <a href={HOTMART_URL} target="_blank" rel="noopener noreferrer"
-                className="border border-foreground rounded-xl p-2 w-fit"
+                className="border border-foreground rounded-xl p-2 w-fit lg:text-2xl"
                 >
                     QUERO GARANTIR O MEU
                 </a>
             </div>
 
-            {/* Imagem */}
-            <div className="flex flex-col gap-4 items-center">
+            {/* Imagens */}
+            <div className="flex flex-col gap-4 items-center lg:w-1/2">
                 <div className="w-90 h-60 relative rounded-[4rem] overflow-hidden">
                     <Image 
                     src={"/ux/section-07-payment/payment-1.webp"}
-                    alt="Sobre a empresa"
+                    alt="Formas de pagamento"
                     fill
                     className="object-cover"
                     />
@@ -32,7 +32,7 @@ export default function Section07Payment() {
                 <div className="w-90 h-60 relative rounded-[4rem] overflow-hidden">
                     <Image 
                     src={"/ux/section-07-payment/payment-2.webp"}
-                    alt="Sobre a empresa"
+                    alt="Acesso ao produto"
                     fill
                     className="object-cover"
                     />
