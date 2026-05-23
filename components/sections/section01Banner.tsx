fe3e6fa4951b4d17a41f66eb03be3cad04160229
Pedro Hammes <predohammes@gmail.com>
2026-05-23 11:20:40 -0300

    style: layout responsivo

--- components/sections/section01Banner.tsx
+++ components/sections/section01Banner.tsx
@@ -11,14 +11,15 @@ export default function Section01Banner() {
             className='object-cover z-0'
             />
             <div className="absolute inset-0 bg-linear-300 from-black/90 pointer-events-none" />
-            <div className='relative z-10 flex flex-col gap-8 justify-center items-center'>
-                <h1 className='font-heading text-8xl text-background text-center'>Social Media Pro</h1>
-                <p className='font-sans text-background text-center text-2xl'>um sistema completo para facilitar e profissionalizar sua gestão de redes sociais</p>
+            <div className='relative z-10 flex flex-col gap-8 justify-center items-center lg:p-10'>
+                <h1 className='font-heading text-8xl lg:text-9xl text-background text-center'>Social Media Pro</h1>
+                <p className='font-sans text-background text-center text-2xl lg:text-4xl'>um sistema completo para facilitar e profissionalizar sua gestão de redes sociais</p>
                 <a href={HOTMART_URL} target="_blank" rel="noopener noreferrer"
                 className='
                 font-sans bg-background text-foreground
                 p-2 rounded-xl w-fit
-                text-base
+                text-base 
+                lg:text-2xl lg:font-bold lg:p-4
                 '>
                     QUERO GARANTIR
                 </a>
